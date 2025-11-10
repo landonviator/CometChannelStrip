28dab55cdd697cbddf88034febc2192b110c592a
javierlemus9 <javierlemus9@gmail.com>
2025-11-10 15:08:40 -0800

    testing

--- source/PluginEditor.cpp
+++ source/PluginEditor.cpp
@@ -1,6 +1,8 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+//test
+
 //==============================================================================
 AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
     : AudioProcessorEditor (&p), processorRef (p)
